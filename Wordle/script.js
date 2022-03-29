570c13df4284e2ed2075c04a0f501ece366b2ef3
Awesomeplayer165 <jacobtrentinidevelopment@gmail.com>
2022-03-29 14:50:24 -0700

    Finished Wordle
    Finished word detection

--- Wordle/script.js
+++ Wordle/script.js
@@ -1,14 +1,28 @@
 var gridItem = [0, 0]
+var words
+
+var hasWon = false
 
 Array.prototype.random = function () {
-    return this[Math.floor((Math.random()*this.length))];
+    return this[Math.floor((Math.random() * this.length))];
 }
 
-var word = (["avant", "hello", "pzazz", "jazzy", "fizzy", "zizit", "ozzie", "scuzz"].map(name => name.toUpperCase())).random()
+if (!(localStorage.getItem("alphabet"))) {
+    alphabet = Array.from(Array(26)).map((e, i) => i + 65).map((x) => String.fromCharCode(x))
+    localStorage.setItem("alphabet", JSON.stringify(alphabet))
+} else {
+    alphabet = JSON.parse(localStorage.getItem("alphabet"))
+}
 
-const alphabet = (Array.from(Array(26)).map((e, i) => i + 65)).map((x) => String.fromCharCode(x));
+var word = (["avant", "hello", "pzazz", "jazzy", "unite", "shows", "parts", "leave"].map(name => name.toUpperCase())).random()
 
-document.addEventListener('keydown', function (event) {
+$.getJSON("words.json", function(data) {
+    words = data
+})
+
+$(document).bind("keydown", function (event) {
+    if (hasWon) { return }
+
     if (alphabet.includes((event.key).toUpperCase()) && gridItem[1] != 5) {
         document.getElementById(`${gridItem[0]},${gridItem[1]}`).innerHTML = (event.key).toUpperCase()
 
@@ -28,26 +42,92 @@ document.addEventListener('keydown', function (event) {
 
     if (event.keyCode == 13 && gridItem[1] == 5) {
 
-        const list = ["0",
-                      "1",
-                      "2",
-                      "3",
-                      "4"]
+        const combined = document.getElementById(`${gridItem[0]},0`).innerHTML +
+                         document.getElementById(`${gridItem[0]},1`).innerHTML +
+                         document.getElementById(`${gridItem[0]},2`).innerHTML +
+                         document.getElementById(`${gridItem[0]},3`).innerHTML +
+                         document.getElementById(`${gridItem[0]},4`).innerHTML
 
-        for (let i = 0; i < 5; i++) {
-            const letterGuess = document.getElementById(`${gridItem[0]},` + list[i])
+        if (combined.toLowerCase() in words) {
 
-            if (letterGuess.innerHTML == word.substring(i, i+1)) {
-                letterGuess.style.backgroundColor = "green"
-            } else if (word.includes(letterGuess.innerHTML)) {
-                letterGuess.style.backgroundColor = "yellow"
-            } else {
-                letterGuess.style.backgroundColor = "gray"
+            var timesGreen = 0
+
+            for (let i = 0; i < 5; i++) {
+                const letterGuess = document.getElementById(`${gridItem[0]},` + String(i))
+    
+                if (letterGuess.innerHTML == word.substring(i, i+1)) {
+                    letterGuess.style.backgroundColor = "green"
+                    timesGreen += 1
+                } else if (word.includes(letterGuess.innerHTML)) {
+                    letterGuess.style.backgroundColor = "yellow"
+                } else {
+                    letterGuess.style.backgroundColor = "gray"
+                }
             }
-        }
+
+            if (timesGreen == 5) {
+                alert("You Win!")
+                hasWon = true
+            }
+        } else {
+            
+            document.getElementById(`${gridItem[0]},0`).innerHTML = ""
+            document.getElementById(`${gridItem[0]},1`).innerHTML = ""
+            document.getElementById(`${gridItem[0]},2`).innerHTML = ""
+            document.getElementById(`${gridItem[0]},3`).innerHTML = ""
+            document.getElementById(`${gridItem[0]},4`).innerHTML = "" 
+
+            gridItem = [gridItem[0] - 1, 0]
+
+            alert("Not a word in the bank")
+       }
 
         gridItem[0] += 1
         gridItem[1] = 0
 
     }
 })
+
+// document.addEventListener('keydown', function (event) {
+//     if (alphabet.includes((event.key).toUpperCase()) && gridItem[1] != 5) {
+//         document.getElementById(`${gridItem[0]},${gridItem[1]}`).innerHTML = (event.key).toUpperCase()
+
+//         gridItem = [gridItem[0], gridItem[1] + 1]
+//     }
+
+//     if (event.keyCode == 8 && gridItem[1] > 0) {
+
+//         if (gridItem[1] == 0) {
+//             gridItem[0] -= 1
+//             gridItem[1] = 5
+//         }
+
+//         document.getElementById(`${gridItem[0]},${gridItem[1]-1}`).innerHTML = ""
+//         gridItem = [gridItem[0], gridItem[1]-1]
+//     }
+
+//     if (event.keyCode == 13 && gridItem[1] == 5) {
+
+//         const list = ["0",
+//                       "1",
+//                       "2",
+//                       "3",
+//                       "4"]
+
+//         for (let i = 0; i < 5; i++) {
+//             const letterGuess = document.getElementById(`${gridItem[0]},` + list[i])
+
+//             if (letterGuess.innerHTML == word.substring(i, i+1)) {
+//                 letterGuess.style.backgroundColor = "green"
+//             } else if (word.includes(letterGuess.innerHTML)) {
+//                 letterGuess.style.backgroundColor = "yellow"
+//             } else {
+//                 letterGuess.style.backgroundColor = "gray"
+//             }
+//         }
+
+//         gridItem[0] += 1
+//         gridItem[1] = 0
+
+//     }
+// })
